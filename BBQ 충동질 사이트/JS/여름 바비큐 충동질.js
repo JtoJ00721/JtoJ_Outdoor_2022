b6684933649a7d7eac4328c056a5d1172a2f3d18
JtoJ00721 <moonlf2000@naver.com>
2022-09-17 16:03:59 +0900

    스크립트까지 완성

---
+++ BBQ 충동질 사이트/JS/여름 바비큐 충동질.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let goto = document.querySelector("#fantasy");
+
+  goto.addEventListener("click", function () {
+    //alert("클릭");
+    document.location.href = "https://www.youtube.com/watch?v=7G-ZuIDgriU&t=0s";
+  });
+});
